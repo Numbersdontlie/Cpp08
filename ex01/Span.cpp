a0c80525348efa278847e2ecbe0cb06abb4ddb98
Numbersdontlie <luifer92@gmail.com>
2025-08-19 14:42:56 +0200

    Adding the methods implementations, to find the min and max span, divide the vector en 1/2 and find min in 1st half and 2nd half then make the diff, the other way around will work for max

--- ex01/Span.cpp
+++ ex01/Span.cpp
@@ -6,7 +6,7 @@
 /*   By: luifer <luifer@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/19 13:37:08 by luifer            #+#    #+#             */
-/*   Updated: 2025/08/19 14:11:26 by luifer           ###   ########.fr       */
+/*   Updated: 2025/08/19 14:41:27 by luifer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,38 @@ Span::Span(const Span &other) : _size(other._size), _setOfNums(other._setOfNums)
         throw SpanFull();
     }
     _setOfNums = other._setOfNums;
+}
+
+Span &Span::operator=(const Span &other) {
+    if (this != &other){
+        _size = other._size;
+        _setOfNums = other._setOfNums;
+    }
+    return *this;
+}
+
+Span::~Span() {}
+
+void Span::addNumber(int n) {
+    if(_setOfNums.size() >= _size){
+        throw SpanFull();
+    }
+    _setOfNums.push_back(n);
+}
+
+//Divide the vector in 2, find the min value in 1st half
+//find the min value in the 2nd half and calculate the difference
+int Span::shortestSpan() const {
+    int min1 = *std::min_element(_setOfNums.begin(), _setOfNums.begin() + _setOfNums.size() / 2);
+    int min2 = *std::min_element(_setOfNums.begin() + _setOfNums.size() / 2, _setOfNums.end());
+    return min2 - min1;
+}
+
+//Divide the vector in 2, find the max value in 1st half
+//find the max value in the 2nd half and calculate the difference
+int Span::longestSpan() const {
+    int max1 = *std::max_element(_setOfNums.begin(), _setOfNums.begin() + _setOfNums.size() / 2);
+    int max2 = *std::max_element(_setOfNums.begin() + _setOfNums.size() / 2, _setOfNums.end());
+    return max2 - max1;
 }
 
